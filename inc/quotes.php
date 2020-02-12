4b1ab636bad4742fe224a302ea092e05eb0bbde0
chapmanCC <chapboy1989@gmail.com>
2020-02-12 23:26:23 +0800

    added quotes

---
+++ inc/quotes.php
@@ -0,0 +1,33 @@
+<?php
+$quotes = array(
+    array(
+        "quote" => "Frankly, my dear, I don't give a damn.",
+        "source" => "Gone With the Wind",
+        "year" => 1939,
+        "citation" => "movie"
+    ),
+    array(
+        "quote" => "I'm going to make him an offer he can't refuse",
+        "source" => "The Godfather",
+        "year" => 1972,
+        "citation" => "movie"
+    ),
+    array(
+        "quote" => "Toto, I've got a feeling we're not in Kansas anymore.",
+        "source" => "The Wizard of Oz",
+        "year" => 1939,
+        "citation" => "movie"
+    ),
+    array(
+        "quote" => "Here's looking at you, kid.",
+        "source" => "Casablanca",
+        "year" => 1942,
+        "citation" => "movie"
+    ),
+    array(
+        "quote" => "May the Force be with you.",
+        "source" => "Star Wars",
+        "year" => 1977,
+        "citation" => "movie"
+    ),
+);
